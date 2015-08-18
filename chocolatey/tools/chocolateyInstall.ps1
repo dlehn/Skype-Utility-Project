cf88691227344cb14d299896b420607cb6f79e3b
David N. Lehn <dlehn@gmx.de>
2015-08-18 21:00:56 +0200

    Chocolatey package update

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName = 'skype-utility-project'
-$url = 'https://github.com/dlehn/Skype-Utility-Project/releases/download/1.3.8/skype_utility_project_binary_1.3.8.zip'
+$url = 'https://github.com/dlehn/Skype-Utility-Project/releases/download/1.4.0/skype_utility_project_binary_1.4.0.zip'
 $skypeRegistry = "HKLM:\Software\Skype"
 if (Test-Path HKLM:\Software\Wow6432Node\Skype)
 {
